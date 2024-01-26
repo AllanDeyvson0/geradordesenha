62ae6ab779d4af04f5594833277b870ac1a3b900
Allan <allandeyvson269@hotmail.com>
2024-01-25 21:23:43 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+//RELACIONANDO ELEMENTOS
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+let containerPassword = document.querySelector("#container-password");
+
+//VARIÁVEIS PARA LÓGICA
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#";
+let novaSenha = "";
+
+//LIGA O TAMANHO DE CARACTERES COM O "RANGE" DO HTML
+sizePassword.innerHTML = sliderElement.value;
+sliderElement.oninput = function(){
+    sizePassword.innerHTML = this.value
+}
+
+//FUNÇÃO GERAR SENHA
+function generatePassword(){
+    let pass = "";
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+
+    //LIMPANDO ÍCONE DE CHECK PÓS COPIAR
+    let copyIcon = document.querySelector("#bi-copy");
+    copyIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" id="bi-copy" class="bi bi-copy" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
+                        </svg>`;
+}
+
+
+//FUNÇÃO COPIAR SENHA
+function copyPassword(){
+    navigator.clipboard.writeText(novaSenha);
+
+    //COLOCANDO O ÍCONE DE CHECK PÓS COPIAR
+    let copyIcon = document.querySelector("#bi-copy");
+    copyIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+        <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+    </svg>`;
+
+    // alert("Senha copiada com sucesso!\nNão compartilhe com ninguém.")
+
+    setTimeout(function(){
+        copyIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" id="bi-copy" class="bi bi-copy" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
+                        </svg>`;
+    }, 3000);
+
+}
+
+
